6eb94746b9a8388821565c7e91cd83c1e1ec71ea
proazad <proazadhossain7595@gmail.com>
2023-08-19 18:23:06 +0300

    Sammer Shoppppppppppppping Design Complete

---
+++ js/utilities.js
@@ -0,0 +1,11 @@
+function textToFloat(text) {
+    return parseFloat(text);
+}
+function getElement(ID) {
+    const element = document.getElementById(ID);
+    return element;
+}
+function createNewElement(tag) {
+    const createNewElement = document.createElement(tag);
+    return createNewElement;
+}
