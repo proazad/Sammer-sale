d5e0ad7b08293a15019fc3053172988afe24d5bc
proazad <proazadhossain7595@gmail.com>
2023-08-19 19:43:48 +0300

    Dicount Option complete And Purchase Complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+
+/**
+ * Calcula Grand Total Price
+ */
 function addToCart(data) {
     // Current / Clicked Product Name 
     const itemName = data.childNodes[3].childNodes[3].innerText;
@@ -29,15 +33,66 @@ function calculateTotalPrice(newItemPrice) {
     const newSubTotalPrice = newItemPrice + oldSubTotalPrice;
     const twoDigit = newSubTotalPrice.toFixed(2);
     subTotalElement.innerText = twoDigit;
-    /**
-     * Calcula Grand Total Price
-     */
-
     const grandTotalElement = getElement("grand-total-price");
     const oldGrandTotalPrice = textToFloat(grandTotalElement);
     const newGrandTotal = newSubTotalPrice.toFixed(2);
     grandTotalElement.innerText = newGrandTotal;
-    
-      
 
+    /**
+ * Dicount Button Enabled 
+ * purchase Button Enabeld Disabled
+*/
+    const button = document.querySelector('#discount-btn');
+    if (newSubTotalPrice < 200) {
+        button.setAttribute('disabled', true);
+    } else {
+        button.removeAttribute('disabled');
+    }
+
+    const purchaseBtn = document.querySelector('#purchase');
+    if (newSubTotalPrice <= 0) {
+        purchaseBtn.setAttribute('disabled', true);
+    } else {
+        purchaseBtn.removeAttribute('disabled');
+    }
+
+}
+
+
+
+function discountCalculation(data) {
+    const coupon = "SELL200";
+    const couponElement = data.previousElementSibling;
+
+    const userInputCoupon = couponElement.value;
+    const totalElement = getElement('sub-total-price');
+    const totalPrice = totalElement.innerText;
+    const grandTotalElement = getElement("grand-total-price");
+    const discountElement = getElement("discount-amount");
+    if (coupon === userInputCoupon) {
+        const discountAmount = ((textToFloat(totalPrice) * 20) / 100);
+        discountElement.innerText = "20% OFF  " + discountAmount;
+        const withDicountGrandTotal = textToFloat(totalPrice) - discountAmount;
+        grandTotalElement.innerText = withDicountGrandTotal;
+    } else {
+        alert("Invalid Coupon Code !!");
+    }
+}
+/**
+ * Dicount Button Enabled 
+ * purchase Button Enabeld Disabled
+*/
+const subTotalElement = getElement('sub-total-price');
+const subTotalPrice2 = parseFloat(subTotalElement.innerText);
+const button = document.querySelector('#discount-btn');
+if (subTotalPrice2 < 200) {
+    button.setAttribute('disabled', true);
+} else {
+    button.removeAttribute('disabled');
+}
+const purchaseBtn = document.querySelector('#purchase');
+if (subTotalPrice2 <= 0) {
+    purchaseBtn.setAttribute('disabled', true);
+} else {
+    purchaseBtn.removeAttribute('disabled');
 }
